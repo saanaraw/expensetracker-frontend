993241c8fce15b6f34d2baa1edc8b03503b1295b
saanaraw <saana.railo@gmail.com>
2020-05-11 13:36:24 +0300

    project initialised, AppNav built

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Home from './Home'
+import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    <Home />,
+
   document.getElementById('root')
 );
 
